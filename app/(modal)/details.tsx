3b6f0b52a178f054d32b6985a98d7ab2c970bfe0
Ken <44661517+kenfj@users.noreply.github.com>
2024-02-06 22:58:20 +0900

    update details page with SectionList

--- app/(modal)/details.tsx
+++ app/(modal)/details.tsx
@@ -1,10 +1,33 @@
 import { restaurant } from '@/assets/data/restaurant';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import Colors from '@/constants/Colors';
+import { Link } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SectionList, SectionListRenderItem, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Details = () => {
+  const DATA = restaurant.food.map((item, index) => ({
+    index,
+    title: item.category,
+    data: item.meals,
+  }));
+
+  const separator = () => <View style={styles.separator} />;
+
+  const renderItem: SectionListRenderItem<any> = ({ item, index }) => (
+    <Link href={'/'} asChild>
+      <TouchableOpacity style={styles.item}>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.dish}>{item.name}</Text>
+          <Text style={styles.dishText}>{item.info}</Text>
+          <Text style={styles.dishText}>${item.price}</Text>
+        </View>
+        <Image source={item.img} style={styles.dishImage} />
+      </TouchableOpacity>
+    </Link>
+  );
+
   return (
     <ParallaxScrollView
       style={styles.container}
@@ -13,7 +36,26 @@ const Details = () => {
       headerText={restaurant.name}
     >
       <View style={styles.detailsContainer}>
-        <Text>Details</Text>
+        <Text style={styles.restName}>{restaurant.name}</Text>
+        <Text style={styles.restDesc}>
+          {restaurant.delivery} {restaurant.tags.map((tag, i) => (
+            `${tag}${i < restaurant.tags.length - 1 ? '・' : ''}`
+          ))}
+        </Text>
+        <Text style={styles.restDesc}>{restaurant.about}</Text>
+
+        <SectionList
+          sections={DATA}
+          scrollEnabled={false}
+          contentContainerStyle={{ paddingBottom: 50 }}
+          keyExtractor={(item, index) => `${item.id + index}`}
+          renderSectionHeader={({ section: { title, index } }) => (
+            <Text style={styles.sectionHeader}>{title}</Text>
+          )}
+          ItemSeparatorComponent={separator}
+          SectionSeparatorComponent={separator}
+          renderItem={renderItem}
+        />
       </View>
     </ParallaxScrollView>
   );
@@ -27,6 +69,46 @@ const styles = StyleSheet.create({
   detailsContainer: {
     height: 2000,
     backgroundColor: Colors.lightGrey,
+  },
+  restName: {
+    fontSize: 30,
+    margin: 16,
+  },
+  restDesc: {
+    fontSize: 16,
+    margin: 16,
+    lineHeight: 22,
+    color: Colors.medium
+  },
+  sectionHeader: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 40,
+    margin: 16,
+  },
+  separator: {
+    marginHorizontal: 16,
+    height: 1,
+    backgroundColor: Colors.grey,
+  },
+  item: {
+    backgroundColor: 'white',
+    padding: 16,
+    flexDirection: 'row',
+  },
+  dishImage: {
+    height: 80,
+    width: 80,
+    borderRadius: 4,
+  },
+  dish: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  dishText: {
+    fontSize: 14,
+    color: Colors.mediumDark,
+    paddingVertical: 4,
   },
 });
 
